cd00c1e985cabb4243b9078c8ace197516c3349d
krit <krittakon159@gmail.com>
2023-01-29 05:18:12 +0700

    update

---
+++ translate.js
@@ -0,0 +1,27 @@
+const { translate } = require("@vitalets/google-translate-api")
+const fs = require("fs")
+
+const words = fs.readFileSync("./3000word.txt", "utf8").trim().split("\n")
+
+let i = 0
+let result_text = []
+if(fs.existsSync("./3000word-th.txt")){
+    result_text = fs.readFileSync("./3000word-th.txt", "utf8").trim().split("\n")
+    
+    i = result_text.length
+}
+
+
+const main = async () => {
+    let word = words[i]
+    const { text } = await translate(word, { to: 'th' });
+    console.log({word, text})
+    result_text.push(text)
+    fs.writeFileSync("./3000word-th.txt", result_text.join("\n"), "utf8")
+    i++
+    if(i<words.length-1){
+        setTimeout(main, 500)
+    }
+}
+
+main()
